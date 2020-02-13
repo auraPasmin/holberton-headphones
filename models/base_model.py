e0ce6f9df790d43628c4b3f8f296a6bc70d685da
auraPasmin <1288@holbertonschool.com>
2020-02-13 17:33:29 -0500

    fix .py

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,19 @@
 import uuid
 import datetime
 
+
 class BaseModel:
-    def __init__(self):
-        
+    """class initialization"""
+    def __init__(self, *args, **kwargs):
+        """ initialization"""
+
+    def __str__(self):
+        """comentario"""
+
+    def save(self):
+        """comentario"""
+
+    def to_dict(self):
+        """ comentario"""
+
+    
