a03d3997ce293936906103523d9f8e28dd99fa7e
auraPasmin <1288@holbertonschool.com>
2020-02-14 09:40:56 -0500

    fix base model add str

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 """ base_model The mold(idea) class is the parent class."""
 import uuid
-import datetime
+from datetime import datetime
 
 
 class BaseModel:
@@ -9,12 +9,13 @@ class BaseModel:
         """ initialization"""
 
     def __str__(self):
-        """comentario"""
+        """should print, and str() should return"""
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.to_dict())
 
     def save(self):
         """comentario"""
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """ comentario"""
-
     
