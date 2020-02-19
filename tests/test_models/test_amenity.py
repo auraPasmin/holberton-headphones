3625d646a3508cbd6bb9c47362cfecddc7d18af1
auraPasmin <1288@holbertonschool.com>
2020-02-19 18:01:13 -0500

    fix add test amenty

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,29 +1,66 @@
 #!/usr/bin/python3
 """ test_amenity test files """
 #imports
-import unittest
+from io import StringIO
+import sys
 import os
+import datetime
+import unittest
+import models
 import pep8
-from models.base_model import BaseModel
+
 from models.amenity import Amenity
 
 class TestAmenity(unittest.TestCase)
-""" test """
-def test_Amenity_inheritence(self):
+    """
+        test
+    """
+    def setUp(self):
         """
-             this is proof that if the kind of amenity inherited from the BaseModel
+            Initializing instance.
         """
-        self.assertIsInstance(Amenity(), BaseModel)
+        self.amenity = Amenity()
 
-    def test_Amenity_attributes(self):
+    def TearDown(self):
         """
-            this is proof that the "Amenity" class had a name attribute.
+            Removing the instance.
         """
-        self.assertTrue("name" in Amenity().__dir__())
+        del self.amenity
 
-    def test_Amenity_attribute_type(self):
-        """
-            this is proof that the Amenity class had the kind of name attribute.
-        """
-        name_value = getattr(Amenity(), "name")
-        self.assertIsInstance(name_value, str)
+    def test_docstring(self):
+        """the test that confirms if everyone has docstring"""
+        msg = "Mod does not docstring"
+        self.assertIsNotNone(models.amenity.__doc__, msg)
+        self.assertIsNotNone(Amenity.__doc__, "Class does not docstring")
+
+    def test_executable_file(self):
+        """test if file has permissions to execute"""
+        # Check if read access
+        is_read_true = os.access('models/amenity.py', os.R_OK)
+        self.assertTrue(is_read_true)
+        # Check if write access
+        is_write_true = os.access('models/amenity.py', os.W_OK)
+        self.assertTrue(is_write_true)
+        # Check if for execution access
+        is_exec_true = os.access('models/amenity.py', os.X_OK)
+        self.assertTrue(is_exec_true)
+
+    def test_is_an_instance(self):
+        """check if base_model is an instance of Amenity"""
+        self.assertIsInstance(self.amenity, Amenity)
+
+    def test_id(self):
+        """test if the id of two instances are different"""
+        base_model_ = Amenity()
+        base_model_1 =Amenity()
+        self.assertNotEqual(base_model_.id, base_model_1.id)
+
+    def test_save(self):
+        """check if the attribute updated_at (date) is updated for
+        the same object with the current date"""
+        base_model_2 =Amenity()
+        first_updated = base_model_2.updated_at
+        base_model_2.save()
+        second_updated = base_model_2.updated_at
+        self.assertNotEqual(first_updated, second_updated)
+
